d8ac0f9aed543f4d4b37812bd884f21f4421c7e2
CHANTYS <SANTHIAGO.R91@GMAIL.COM>
2026-05-07 00:54:19 -0300

    ADA/USDT token. Buy periodically regardless of the price.

---
+++ BotDiario.py
@@ -0,0 +1,38 @@
+API_KEY = 'eQRE2IoS3ozOzYHgDzLF4P3nOkIEVoMZtkVjlzRWURnbqKEpkbTYoSXP5silKN6a'
+SECRET = 'lFXZunP4Od3MXr7BdSIo1l8cbjdOKvmnNpw38zuEH0Sa8plR12EOEqdlN7EJjCaX'
+
+BASE_URL = 'https://testnet.binance.vision/key/api/v3/order'
+
+from datetime import datetime
+import requests
+import hashlib
+import hmac
+import time
+
+
+quantity = 20
+
+symbol = 'ADAUSDT'
+
+while True:
+    params = {
+        'symbol': symbol,
+        'side': 'BUY',
+        'type': 'MARKET',
+        'quantity': quantity,
+        'timestamp': int(time.time() * 1000)
+    }
+
+    query_string = '&'.join([f'{key}={params[key]}' for key in params])
+
+    signature = hmac.new(SECRET.encode('utf-8'), query_string.encode('utf-8'), hashlib.sha256).hexdigest()
+
+    params['signature'] = signature
+
+    response = requests.post(BASE_URL, params=params, headers={'X-MBX-APIKEY': API_KEY})
+
+    now = datetime.now()
+
+    print(f'Compra de {quantity} {symbol} efectuada {now.strftime("%d/%m/%Y, %H:%M:%S")}')
+
+    time.sleep(24*3600)
